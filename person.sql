241b119dedd06eb5edab2b257b0abff9500d6327
Mathis Van Eetvelde <mathis.vaneetvelde@gmail.com>
2020-12-16 23:53:07 +0100

    Improved person.SQL

--- person.sql
+++ person.sql
@@ -1,4 +1,13 @@
+CREATE USER 'user1'@'%' IDENTIFIED BY 'password';
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON *.* TO 'user1'@'%';
+
+FLUSH PRIVILEGES;
+
+CREATE DATABASE test;
+
 USE `test`;
+
 CREATE TABLE IF NOT EXISTS `person` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `Name` text,
